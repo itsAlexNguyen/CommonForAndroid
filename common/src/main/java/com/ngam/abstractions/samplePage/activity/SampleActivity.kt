5409ae69c438a71049f2f6ccfb963a35e5c10900
Rishi-B <33183390+Rishi-B@users.noreply.github.com>
2018-07-11 20:17:35 -0400

    Added Abstract Fragment

--- common/src/main/java/com/ngam/abstractions/samplePage/activity/SampleActivity.kt
+++ common/src/main/java/com/ngam/abstractions/samplePage/activity/SampleActivity.kt
@@ -1,20 +1,33 @@
 package com.ngam.abstractions.samplePage.activity
 
-import android.widget.Toast
+import android.content.Intent
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import com.ngam.abstractions.R
 import com.ngam.abstractions.abstractions.AbstractActivity
-import com.ngam.abstractions.abstractions.AbstractMenuActivity
+import com.ngam.abstractions.abstractions.AbstractClassProperties
+import com.ngam.abstractions.abstractions.AbstractFragment
 import com.ngam.abstractions.samplePage.adapter.SampleAdapter
 import com.ngam.abstractions.samplePage.contracts.SampleView
+import com.ngam.abstractions.samplePage.fragment.SampleFragment
 import com.ngam.abstractions.samplePage.presenter.SamplePresenter
 
-class SampleActivity: AbstractMenuActivity<SamplePresenter, SampleAdapter>(), SampleView {
-    override fun setProperties(): ActivityProperties<SamplePresenter, SampleAdapter> {
+class SampleActivity: AbstractActivity<SamplePresenter, SampleAdapter>(), SampleView {
+    override fun setProperties(): AbstractClassProperties<SamplePresenter, SampleAdapter> {
         presenter = SamplePresenter(this)
         adapter = SampleAdapter(presenter)
-        return ActivityProperties(presenter, adapter, "Sample Title")
-    }
-
-    override fun makeToast(toastString: String) {
-        Toast.makeText(this, toastString, Toast.LENGTH_SHORT).show()
+        return AbstractClassProperties(presenter, adapter, "Sample Title")
     }
 }
+
+// To launch Fragment:
+//class SampleActivity: AppCompatActivity(), SampleView {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        val sampleFragment: SampleFragment = SampleFragment()
+//        supportFragmentManager.beginTransaction()
+//                .add(sampleFragment, "Sample Fragment")
+//                .commit()
+//    }
+//}
